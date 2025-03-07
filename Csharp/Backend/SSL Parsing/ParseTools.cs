b25e1192b6eabe2ed847af0738e32dd1e59799e9
Varrox <140557931+Varrox@users.noreply.github.com>
2025-03-07 16:19:30 -0500

    l

--- Csharp/Backend/SSL Parsing/ParseTools.cs
+++ Csharp/Backend/SSL Parsing/ParseTools.cs
@@ -5,6 +5,12 @@ namespace SSLParser
 {
     public class ParsingTools
     {
+        /// <summary>
+        /// Parses functions()
+        /// </summary>
+        /// <param name="line">the line to parse</param>
+        /// <param name="method">the name of the method</param>
+        /// <returns>all arguments inside the parenthases of the method</returns>
         public static string[] GetInParenthases(string line, out string method)
         {
             int index = line.IndexOf('(');
@@ -68,20 +74,56 @@ namespace SSLParser
             return text.Split("//")[0].Trim();
         }
 
+        /// <summary>
+        /// Sets a variable of a string name, to a value from a string.
+        /// </summary>
+        /// <param name="variable">variable to be set</param>
+        /// <param name="value">value to set the variable</param>
+        /// <param name="objectType">the type the variable is from</param>
+        /// <param name="obj">the object that is affected</param>
         public static void SetVariable(string variable, string value, ref Type objectType, object obj)
         {
-            foreach (FieldInfo field in objectType.GetFields(BindingFlags.Public))
+            FieldInfo field = objectType.GetField(variable);
+            if (field != null)
             {
-                if (field.Name == variable)
-                {
-                    switch(field.GetType())
-                    {
+                object newValue = ConvertType(value, field.FieldType);
 
-                    }
-                    
-                    // Add type filtering
-                    field.SetValue(obj, value);
-                }
+                if (newValue != null)
+                    field.SetValue(obj, newValue);
+                else
+                    Debug.ErrorLog($"{variable} is not a variable that can be set properly in SSL");
+            }
+            else
+            {
+                Debug.ErrorLog($"{variable} is not a variable, maybe look over it again");
+            }
+        }
+
+        public static object ConvertType(string value, Type type)
+        {
+            if (type is string)
+            {
+                return value;
+            }
+            else if (type is int)
+            {
+                return Convert.ToInt32(value);
+            }
+            else if (type is long)
+            {
+                return Convert.ToInt64(value);
+            }
+            else if (type is float)
+            {
+                return Convert.ToSingle(value);
+            }
+            else if (type is double)
+            {
+                return Convert.ToDouble(value);
+            }
+            else
+            {
+                return null;
             }
         }
     }
