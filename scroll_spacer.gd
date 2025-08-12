a07e0181fe2fbea7cea90bc94b3ab0d4b47e9fad
Varrox <140557931+Varrox@users.noreply.github.com>
2025-08-12 16:24:27 -0400

    Added spacing to scrollbar and content

---
+++ scroll_spacer.gd
@@ -0,0 +1,7 @@
+extends VSeparator
+
+@export var scroll_container:ScrollContainer
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	visible = scroll_container.get_v_scroll_bar().visible
