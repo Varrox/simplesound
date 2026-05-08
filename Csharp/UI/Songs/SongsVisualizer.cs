97b09fac93a9fc023eacddd87cab08a6f8604668
Varrox <140557931+Varrox@users.noreply.github.com>
2026-05-08 14:18:24 -0400

    Fixed some stuff

--- Csharp/UI/Songs/SongsVisualizer.cs
+++ Csharp/UI/Songs/SongsVisualizer.cs
@@ -14,14 +14,14 @@ public partial class SongsVisualizer : ScrollContainer
     int last_scroll = -1, last_first_song = -1;
     Vector2 last_size = Vector2.Zero;
 
-    SongDisplay[] song_displays;
+    List<SongDisplay> song_displays;
     Dictionary<int, SongData> song_datas;
 
     const int min_y = 60;
     int separation = 0, top_card_y, single_button_size;
     int GetTotalHeight()
     {
-        return (single_button_size * (Globals.main.playlists[Globals.main.looked_at_playlist].songs.Count + 2));
+        return (single_button_size * Globals.main.playlists[Globals.main.looked_at_playlist].songs.Count);
     }
 
     public override void _Ready()
@@ -32,6 +32,7 @@ public partial class SongsVisualizer : ScrollContainer
         Globals.main.playlist_visualizer.OnSelectPlaylist += LoadPlaylist;
 
         song_datas = new Dictionary<int, SongData>();
+        song_displays = new List<SongDisplay>();
     }
 
     public override void _Process(double delta)
@@ -43,115 +44,116 @@ public partial class SongsVisualizer : ScrollContainer
         
         if (last_scroll != ScrollVertical || last_size != Size)
         {
-            int max_displays = (int)((int)Size.Y / (float)single_button_size + 1f) + 1;
+            int max_displays = (int)Mathf.Ceil(Size.Y / single_button_size) + 1;
+            int target_displays = Mathf.Min(max_displays, playlist.songs.Count);
             (int top, int bottom) = CalculateSize();
             int first_song = GetFirstSong();
 
             top_spacer.CustomMinimumSize = Vector2.Down * top;
             bottom_spacer.CustomMinimumSize = Vector2.Down * bottom;
+            
+            bottom_spacer.Visible = target_displays == max_displays;
+            top_spacer.Visible = target_displays == max_displays;
 
-            if (last_size != Size)
+            // Check if there is enough displays, if not, add or remove them. when adding a new one, update it.
+
+            if(song_displays.Count != target_displays) // NOTE: Is only true if there is more songs added (only when previous count was below max_displays), or the size changed.
             {
-                if(song_displays == null)
+                if(song_displays.Count > target_displays) // Delete old
                 {
-                    song_displays = new SongDisplay[max_displays];
-                    for(int i = 0; i < song_displays.Length; i++)
+                    for(int i = song_displays.Count; i >= target_displays; i--)
                     {
-                        song_displays[i] = template.Instantiate() as SongDisplay;
+                        song_displays[i].QueueFree();
+                        song_displays.RemoveAt(i);
+                    }
+                }
+                else // Add new
+                {
+                    for(int i = song_displays.Count; i < target_displays; i++)
+                    {
+                        song_displays.Add(template.Instantiate() as SongDisplay);
                         container.AddChild(song_displays[i]);
+                        container.MoveChild(song_displays[i], container.GetChildCount() - 2);
 
-                        SongData data = new SongData(playlist.songs[first_song + i]);
-                        song_displays[i].Init(first_song + i, data, playlist.type, playlist.type == Playlist.PlaylistType.Album ? null : ConvertToGodot.GetCover(playlist.songs[first_song + i]));
+                        int song = first_song + i;
+
+                        SongData data = GetSongData(song, playlist);
+                        song_displays[i].Init(song, data, playlist.type, (playlist.type == Playlist.PlaylistType.Album) ? null : ConvertToGodot.GetCover(playlist.songs[song]));
                     }
                 }
-                else if(song_displays.Length != max_displays)
-                {
-                    if(song_displays.Length < max_displays)
-                    {
-                        SongDisplay[] displays = new SongDisplay[max_displays];
-                        song_displays.CopyTo(displays, 0);
-                        song_displays = displays;
-
-                        for(int i = 0; i < song_displays.Length; i++)
-                        {
-                            if(!container.GetChildren().Contains(song_displays[i]))
-                            {
-                                song_displays[i] = template.Instantiate() as SongDisplay;
-                                container.AddChild(song_displays[i]);
-                            }
-                        }
-                    }
-                    else if(song_displays.Length > max_displays)
-                    {
-                        for(int i = song_displays.Length - max_displays - 1; i >= 0; i--)
-                        {
-                            song_displays[i].QueueFree();
-                        }
-
-                        SongDisplay[] displays = new SongDisplay[max_displays];
-                        song_displays.CopyTo(displays, 0);
-                        song_displays = displays;
-                    }
-                }
-
-                container.MoveChild(bottom_spacer, container.GetChildCount() - 1);
             }
 
-            if (first_song != last_first_song)
+            // If first song has changed, go through and update only the ones that need new covers and data, and shift back old displays.
+
+            int shift = first_song - last_first_song; // The amount to shift by
+            int a_shift = Mathf.Abs(shift);
+
+            if(shift != 0 && ScrollVertical > top_card_y + single_button_size) // NOTE: Is only true if there has been any scrolling, and past the beginning.
             {
-                int dt = first_song - last_first_song;
-                bool f = dt > 0;
-                bool[] set = new bool[song_displays.Length];
+                // If shift is less than 0, then the user has scrolled up.
+                // If shift is more than 0, then the user has scrolled down.
 
-                for(int i = 0; i < song_displays.Length; i++)
+                // Scrolling up, requires the last song display to be placed at the beginning, and updated.
+                // Scrolling down, requires the first song display to be placed at the end, and updated.
+
+                // Do these ^^^ (|shift|) times
+
+                int last_index = song_displays.Count - 1;
+
+                for(int i = 0; i < a_shift; i++)
                 {
-                    int song = first_song + i;
+                    SongDisplay display;
+                    int index;
 
-                    if(!song_datas.ContainsKey(song))
+                    if(shift < 0)
                     {
-                        SongData data = new SongData(playlist.songs[song]);
-                        song_datas[song] = data;
+                        display = song_displays[last_index];
+                        song_displays.RemoveAt(last_index);
+                        song_displays.Insert(0, display);
 
-                        song_displays[i].Init(song, song_datas[song], playlist.type, (playlist.type == Playlist.PlaylistType.Album) ? null : ConvertToGodot.GetCover(playlist.songs[song]));
-                        set[i] = true;
+                        container.MoveChild(display, 1);
+
+                        index = (a_shift - i) - 1;
                     }
+                    else
+                    {
+                        display = song_displays[0];
+                        song_displays.RemoveAt(0);
+                        song_displays.Add(display); // Add instead of insert because it is probably faster and makes more sense logic wise.
+
+                        container.MoveChild(display, last_index - 1);
+
+                        index = song_displays.Count - (a_shift - i);
+                    }
+
+                    int song = first_song + index;
+
+                    SongData data = GetSongData(song, playlist);
+                    song_displays[index].Init(song, data, playlist.type, (playlist.type == Playlist.PlaylistType.Album) ? null : ConvertToGodot.GetCover(playlist.songs[song]));
                 }
-
-                Texture2D last_cover = null;
-
-                for(int i = 0; i < song_displays.Length; i++)
-                {
-                    if(set[i])
-                    {
-                        last_cover = song_displays[i].cover.Texture;
-                        continue;
-                    }
-
-                    int last_index = i - dt;
-                    int song = first_song + i;
-
-                    if(last_index != -1 && last_index != song_displays.Length) // Swap
-                    {
-                        if((i == 0 && !f) || (i == song_displays.Length - 1 && f))
-                        {
-                            song_displays[i].Init(song, song_datas[song], playlist.type, (playlist.type == Playlist.PlaylistType.Album) ? null : ConvertToGodot.GetCover(playlist.songs[song]));
-                            last_cover = song_displays[i].cover.Texture;
-                        }
-                        else
-                        {
-                            Texture2D cover = song_displays[last_index].cover.Texture;
-                            song_displays[i].Init(last_index, song_datas[last_index], playlist.type, (playlist.type == Playlist.PlaylistType.Album) ? null : last_cover);
-                            last_cover = cover;
-                        }
-                    }
-                }
-                
-                last_first_song = first_song;
             }
 
             last_scroll = ScrollVertical;
             last_size = Size;
+            last_first_song = first_song;
         }
+    }
+
+    private SongData GetSongData(int song, in Playlist playlist)
+    {
+        if (!song_datas.ContainsKey(song))
+        {
+            if(song < playlist.songs.Count)
+            {
+                SongData data = new SongData(playlist.songs[song]);
+                song_datas[song] = data;
+            }
+            else
+            {
+                song_datas[song] = new SongData{};
+            }
+        }
+        return song_datas[song];
     }
 
     private void LoadPlaylist(int playlist_index, Texture2D playlist_cover)
@@ -190,10 +192,10 @@ public partial class SongsVisualizer : ScrollContainer
         return (int)MathF.Max((top) / single_button_size, 0);
     }
 
-    public void UpdateSong(int index, SongData song_data, Texture2D cover)
+    public void UpdateSong(int index, in SongData song_data, in Texture2D cover)
     {
         int first_song = GetFirstSong();
-        if (Globals.main.looked_at_playlist != Globals.main.playlist_index || index < first_song || index >= (song_displays.Length + first_song))
+        if (Globals.main.looked_at_playlist != Globals.main.playlist_index || index < first_song || index >= (song_displays.Count + first_song))
             return;
 
         song_displays[index - first_song].Init(index, song_data, Globals.main.playlists[Globals.main.looked_at_playlist].type, cover);
--- Styling/background_image_transition.gd
+++ Styling/background_image_transition.gd
@@ -4,28 +4,28 @@ extends SubViewport
 @export var target_texture_rect:TextureRect
 
 var transition_value:float
-var target_transition_value:float
 
 var target_texture:Texture2D:
 	set(value):
 		past_target_texture = target_texture
 		target_texture = value
 		
-		transition_value = 0.0
-		target_transition_value = 1.0
-
-var past_target_texture:Texture2D
-
-func _process(_delta: float) -> void:
-	var shader:ShaderMaterial = transitioner.material as ShaderMaterial
-	
-	if transition_value != 1.0:
-		transition_value = Helper.smooth(transition_value, target_transition_value, 4)
+		var shader:ShaderMaterial = transitioner.material as ShaderMaterial
 		
 		shader.set_shader_parameter("past_image", past_target_texture)
 		shader.set_shader_parameter("new_image", target_texture)
-		shader.set_shader_parameter("t", transition_value)
 		
-		target_texture_rect.texture = get_texture()
+		transition_value = 0.0 # Reset
+
+var past_target_texture:Texture2D
+
+func _ready() -> void:
+	target_texture_rect.texture = get_texture()
+
+func _process(_delta: float) -> void:
+	if transition_value != 1.0:
+		transition_value = Helper.smooth(transition_value, 1.0, 4)
+		
+		(transitioner.material as ShaderMaterial).set_shader_parameter("t", transition_value)
 	
 	transitioner.process_mode = Node.PROCESS_MODE_ALWAYS if transition_value != 1.0 else Node.PROCESS_MODE_DISABLED
