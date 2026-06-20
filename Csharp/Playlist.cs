021a97e1889181df397b82954980a5bbda340380
Varrox <140557931+Varrox@users.noreply.github.com>
2026-06-20 01:49:35 -0400

    Reformatted Playlist file & removed unused struct

--- Csharp/Playlist.cs
+++ Csharp/Playlist.cs
@@ -8,9 +8,8 @@ public class Playlist
     public string name, cover;
     public List<string> songs;
 
-    public string GetPath()
-    {
-        return path != null ? path : System.IO.Path.Combine(SaveSystem.USER_DATA, "Playlists", $"{name.Replace('\\', '-').Replace('/', '-').Replace(':', '-')}.json");
+    public string GetPath() {
+        return path ?? System.IO.Path.Combine(SaveSystem.USER_DATA, "Playlists", $"{name.Replace('\\', '-').Replace('/', '-').Replace(':', '-')}.json");
     }
 
     private string path;
@@ -19,8 +18,8 @@ public class Playlist
     public CustomInfo custom_info;
 
     public PlaylistType type = PlaylistType.Default;
-    public enum PlaylistType
-    {
+
+    public enum PlaylistType {
         Default,
         Album
     }
@@ -29,79 +28,58 @@ public class Playlist
     /// Saves to path
     /// </summary>
     /// <returns></returns>
-    public string Save()
-    {
+    public string Save() {
         File.WriteAllText(GetPath(), JsonConvert.SerializeObject(this, Formatting.Indented));
         return GetPath();
     }
 
-    public void DeleteFile()
-    {
+    public void DeleteFile() {
         File.Delete(GetPath());
         Globals.save_data.playlists.Remove(GetPath());
         Globals.save_data.Save();
     }
 
-    public Playlist(string name, string coverpath, List<string> songs)
-    {
+    public Playlist(string name, string cover, List<string> songs) {
         this.name = name;
-        this.cover = coverpath;
+        this.cover = cover;
         this.songs = ProcessSongs(songs);
         custom_info = new CustomInfo();
     }
 
-    public static Playlist LoadFromFile(string path)
-    {
+    public static Playlist LoadFromFile(string path) {
         Playlist playlist = JsonConvert.DeserializeObject<Playlist>(File.ReadAllText(path));
         playlist.songs = ProcessSongs(playlist.songs);
         return playlist;
     }
 
-    public static List<string> ProcessSongs(List<string> songs)
-    {
-        for(int i = 0; i < songs.Count; i++)
-        {
-            if(!File.Exists(songs[i]))
-            {
+    public static List<string> ProcessSongs(List<string> songs) {
+        for(int i = 0; i < songs.Count; i++) {
+            if(!File.Exists(songs[i])) {
                 string path = songs[i];
                 songs.RemoveAt(i);
                 
-                if (Directory.Exists(path))
-                {
-                    songs.InsertRange(i, GetSongsFromDirectory(path));
-                }
+                if (Directory.Exists(path)) songs.InsertRange(i, GetSongsFromDirectory(path));
             }
         }
 
         return songs;
     }
 
-    public static List<string> GetSongsFromDirectory(string directory)
-    {
+    public static List<string> GetSongsFromDirectory(string directory) {
         List<string> songs = new List<string>();
 
-        foreach(string d in Directory.GetDirectories(directory))
-        {
-            if (Directory.Exists(d))
-            {
-                songs.AddRange(GetSongsFromDirectory(d));
-            }
+        foreach(string dir in Directory.GetDirectories(directory)) {
+            if (Directory.Exists(dir)) songs.AddRange(GetSongsFromDirectory(dir));
         }
 
-        foreach(string s in Directory.GetFiles(directory))
-        {
-            
-            if(Tools.ValidAudioFile(s))
-            {
-                songs.Add(s);
-            }
+        foreach(string file in Directory.GetFiles(directory)) {
+            if(Tools.ValidAudioFile(file)) songs.Add(file);
         }
 
         return songs;
     }
 
-    [JsonConstructor] public Playlist()
-    {
+    [JsonConstructor] public Playlist() {
         
     }
 }
@@ -109,11 +87,4 @@ public class Playlist
 public struct CustomInfo
 {
     public string overlay_color, background_path;
-}
-
-public struct UICustomization
-{
-    public string default_font;
-    public string default_color;
-    public string default_font_color;
 }
