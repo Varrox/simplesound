1e538ef045ac23aee761e8d09905de9bcfa696d2
Varrox <140557931+Varrox@users.noreply.github.com>
2026-06-22 21:35:29 -0400

    Reformatted Player

--- Csharp/Player.cs
+++ Csharp/Player.cs
@@ -21,8 +21,7 @@ public partial class Player : Node
     Texture2D playlist_icon;
     int playlist_icon_index = -1;
 
-    public override void _Ready()
-	{
+    public override void _Ready() {
         loop.ButtonUp += SetLoop;
         shuffle.ButtonUp += SetShuffle;
         play.ButtonUp += Globals.main.Play;
@@ -50,41 +49,31 @@ public partial class Player : Node
     public void SetShuffleTexture() => shuffle.Icon = Globals.main.shuffled ? Globals.shuffle_on_texture : Globals.shuffle_off_texture;
     public void SetMuteTexture() => mute_button.Icon = muted ? Globals.unmute_texture : Globals.mute_texture;
 
-    public void MuteVolume()
-    {
+    public void MuteVolume() {
         muted = !muted;
         SetMuteTexture();
-        if (muted)
-        {
+        if (muted) {
             muted_volume = Mathf.Max(volume_slider.Value, -49);
-            if (Globals.main.audio_player.VolumeDb == -80f)
-            {
+            if (Globals.main.audio_player.VolumeDb == -80f) {
                 muted = false;
                 SetMuteTexture();
                 volume_slider.Value = muted_volume;
             }
-            else
-            {
+            else {
                 Globals.main.audio_player.VolumeDb = -80f;
                 volume_slider.Value = -50;
             }
         }
-        else
-        {
-            volume_slider.Value = muted_volume;
-        }
+        else volume_slider.Value = muted_volume;
     }
 
-    public void VolumeUnmute()
-    {
+    public void VolumeUnmute() {
         muted = false;
         SetMuteTexture();
     }
 
-    public bool Interrupt()
-    {
-        if(!interrupted)
-        {
+    public bool Interrupt() {
+        if(!interrupted) {
             if (Globals.main.playing) Globals.main.Play();
             interrupted = true;
             return true;
@@ -92,8 +81,7 @@ public partial class Player : Node
         return false;
     }
 
-    public void SetShuffle()
-    {
+    public void SetShuffle() {
         Globals.main.shuffled = !Globals.main.shuffled;
 
         Globals.main.offset = Globals.main.song_index;
@@ -102,26 +90,19 @@ public partial class Player : Node
         shuffle.Icon = Globals.main.shuffled ? Globals.shuffle_on_texture : Globals.shuffle_off_texture;
     }
 
-    public void SetLoop()
-    {
+    public void SetLoop() {
         Globals.main.loop = !Globals.main.loop;
         loop.Icon = Globals.main.loop ? Globals.loop_on_texture : Globals.loop_off_texture;
     }
 
     public void SetPlayIcon(bool playing) => play.Icon = !playing || !Globals.main.IsSongAvailable() ? Globals.play_texture : Globals.pause_texture;
 
-    public void Move(int by)
-    {
-        if (!interrupted)
-        {
-            Globals.main.MoveSong(by);
-        }
+    public void Move(int by) {
+        if (!interrupted) Globals.main.MoveSong(by);
     }
 
-    public void OnLoadSong()
-    {
-        if (Globals.main.IsSongAvailable())
-        {
+    public void OnLoadSong() {
+        if (Globals.main.IsSongAvailable()) {
             string name = Tools.GetMediaTitle(Globals.main.song);
             song_name.Text = name;
             song_name.TooltipText = name;
@@ -133,18 +114,15 @@ public partial class Player : Node
             Texture2D cover = ConvertToGodot.GetSongCover(Globals.main.song);
             song_cover.Texture = cover;
 
-            if (cover == Globals.default_cover && Globals.main.playlist.type == Playlist.PlaylistType.Album)
-            {
-                if(playlist_icon == null || playlist_icon_index != Globals.main.playlist_index)
-                {
+            if (cover == Globals.default_cover && Globals.main.playlist.type == Playlist.PlaylistType.Album) {
+                if(playlist_icon == null || playlist_icon_index != Globals.main.playlist_index) {
                     playlist_icon = ConvertToGodot.LoadImageFromFile(Globals.main.playlist.cover) ?? Globals.default_cover;
                     playlist_icon_index = Globals.main.playlist_index;
                 }
                 
                 song_cover.Texture = playlist_icon;
             }
-            else
-            {
+            else {
                 song_cover.Texture = cover;
             }
             
@@ -156,8 +134,7 @@ public partial class Player : Node
             progress.MaxValue = Globals.main.audio_player.Stream.GetLength();
             progress.Editable = true;
         }
-        else
-        {
+        else {
             song_name.Text = "No song playing";
             song_name.TooltipText = "";
             song_artist.Text = "No artist";
@@ -176,33 +153,26 @@ public partial class Player : Node
         Globals.discord.UpdateSong();
     }
 
-    public void SetTime(bool value)
-    {
+    public void SetTime(bool value) {
         Globals.main.time = (float)progress.Value;
-        if (Globals.main.playing)
-        {
+        if (Globals.main.playing) {
             Globals.main.audio_player.Play(Globals.main.time);
 
-            if (Globals.main.video_player.Stream != null)
-            {
+            if (Globals.main.video_player.Stream != null) {
                 Globals.main.video_player.StreamPosition = Globals.main.time;
             }
         }
         
         can_set_time = false;
     }
-    public void ApplyPlaylistSettings()
-    {
-        if(Globals.main.playlist == null)
-            return;
+    public void ApplyPlaylistSettings() {
+        if(Globals.main.playlist == null) return;
 
         background_color = Globals.main.playlist.custom_info.overlay_color != null ? Color.FromHtml(Globals.main.playlist.custom_info.overlay_color) : new Color(0.0f, 0.0f, 0.0f, 0.0f);
     }
 
-    public override void _Process(double delta)
-	{
-        if(!interrupted)
-        {
+    public override void _Process(double delta) {
+        if(!interrupted) {
             if(Input.IsActionJustPressed("play")) Globals.main.Play();
             else if (Input.IsActionJustPressed("next")) Move(1);
             else if (Input.IsActionJustPressed("back")) Move(-1);
@@ -210,22 +180,18 @@ public partial class Player : Node
 
         if (Globals.main.audio_player.Stream != null) current_time.Text = Tools.SecondsToTimestamp(Globals.main.time);
 
-        if (!can_set_time)
-        {
+        if (!can_set_time) {
             progress.Value = Globals.main.time;
         }
-        else if (!Globals.main.playing)
-        {
+        else if (!Globals.main.playing) {
             Globals.main.time = (float)progress.Value;
         }
 
-        if (Globals.main.IsSongAvailable())
-        {
+        if (Globals.main.IsSongAvailable()) {
             float max = 0.65f;
             background_color_rect.Color = background_color_rect.Color.Lerp(background_color.Clamp(new Color(), new Color(max, max, max, max)), (float)delta * 2f);
 
-            if (!muted)
-            {
+            if (!muted) {
                 Globals.main.audio_player.VolumeDb = (float)(volume_slider.Value != -50 ? volume_slider.Value : -80);
                 mute_button.Icon = Globals.main.audio_player.VolumeDb == -80 ? Globals.unmute_texture : Globals.mute_texture;
             }
